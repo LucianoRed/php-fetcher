cea0f5ae6916fd42f555eaadaf1f3d714b90e90c
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-11-17 12:28:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
-function calcularTempoBusca($url) {
+function buscarArquivoJSON($url) {
     $inicio = microtime(true); // Captura o tempo inicial em microssegundos
 
-    // Faz a requisição para a URL
-    $conteudo = file_get_contents($url);
+    // Faz a requisição para a URL para obter o arquivo JSON
+    $conteudoJSON = file_get_contents($url);
 
     $fim = microtime(true); // Captura o tempo final em microssegundos
 
     $tempo = $fim - $inicio; // Calcula o tempo decorrido
-    return $tempo;
+
+    // Decodifica o conteúdo JSON para um objeto PHP
+    $objetoJSON = json_decode($conteudoJSON);
+
+    return [
+        'tempo' => $tempo,
+        'objetoJSON' => $objetoJSON
+    ];
 }
 
-// URL que você deseja buscar
+// URL que você deseja buscar (um arquivo JSON)
 $url = getenv("URL");
 
-// Chamada da função para calcular o tempo de busca
-$tempoDeBusca = calcularTempoBusca($url);
+// Chamada da função para buscar o arquivo JSON
+$resultadoBusca = buscarArquivoJSON($url);
 
-echo "Tempo para buscar a URL: " . number_format($tempoDeBusca, 5) . " segundos";
+echo "Tempo para buscar o arquivo JSON: " . number_format($resultadoBusca['tempo'], 5) . " segundos<br>";
+
+echo "Objeto JSON:<br>";
+echo "<pre>";
+print_r($resultadoBusca['objetoJSON']);
+echo "</pre>";
 ?>
+
